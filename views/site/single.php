475f02056e40acc83c145e5c94479290e8410fdb
Ramil Nizamutdinov <poshtamoya@mail.ru>
2019-02-14 13:25:20 +0500

    second commit

---
+++ views/site/single.php
@@ -0,0 +1,38 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Страница новости';
+?>
+<div class="col-md-12 content-page">
+  <div class="col-md-12 blog-post">
+
+    
+    <!-- Post Headline Start -->
+    <div class="post-title">
+      <h1>How to make your company website based on bootstrap framework on localhost?</h1> 
+    </div>
+       <!-- Post Headline End -->
+        
+        
+    <!-- Post Detail Start -->
+    <div class="post-info">
+      <span>November 23, 2016 / by <a href="#" target="_blank">Alex Parker</a></span>
+    </div>
+       <!-- Post Detail End -->
+        
+        
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at quam at orci commodo hendrerit vitae nec eros. Vestibulum neque est, imperdiet nec tortor nec, tempor semper metus. <b>Cras vel tempus velit</b>, et accumsan nisi. Duis laoreet pretium ultricies. Curabitur rhoncus auctor nunc congue sodales. Sed posuere nisi ipsum, eget dignissim nunc dapibus eget. Aenean elementum <b><a href="javascript:void(0)" data-toggle="popover" data-placement="top" data-content="You can write any text here">Click me</a></b> sollicitudin sapien ut sapien fermentum aliquet mollis. Curabitur ac quam orci sodales quam ut tempor. <b data-toggle="tooltip" data-placement="top" title="You can write any text here.">Hover me</b> suspendisse, gravida in augue in, interdum bibendum dui. Suspendisse sit amet justo sit amet diam fringilla commodo. Praesent ac magna at metus malesuada tincidunt non ac arcu. Nunc gravida eu felis vel elementum. Vestibulum sodales quam ut tempor tempor. Donec sollicitudin sapien ut sapien fermentum, non ultricies nulla gravida.</p>
+        
+    
+    <!-- Post Image Start -->
+    <div class="post-image margin-top-40 margin-bottom-40">
+      <img src="images/blog/1.jpg" alt="">
+      <p>Image source from <a href="#" target="_blank">Link</a></p>                                      
+    </div>
+      <!-- Post Image End -->
+      
+      
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at quam at orci commodo hendrerit vitae nec eros. Vestibulum neque est, imperdiet nec tortor nec, tempor semper metus. Cras vel tempus velit, et accumsan nisi. Duis laoreet pretium ultricies. Curabitur rhoncus auctor nunc congue sodale Sed posuere nisi ipsum.</p>
+  </div>
+</div>
